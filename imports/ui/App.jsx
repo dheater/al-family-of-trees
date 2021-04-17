5deca279691afd4d1520558d78122a20a7a3ab81
Daniel Heater <daniel@heatertech.com>
2021-04-17 17:43:38 -0500

    Change the title

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
+    <h1>Welcome to AL Family of Trees: Champion's Edition!</h1>
     <Info/>
   </div>
 );
