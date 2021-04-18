b7e8cd1a226fe30067fd6fd051f70ce656ae31b1
Daniel Heater <daniel@heatertech.com>
2021-04-17 22:10:29 -0500

    Use spacebar with blaze templating engine

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
+import { Template } from 'meteor/templating'
+
+import './main.html';
 
 Meteor.startup(() => {
   render(<App/>, document.getElementById('react-target'));
 });
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.myParagraph.helpers({
+    text: 'This is paragraph...'
+  });
+}
