21ea7efcc26c672af2525d571a1f62bab89b041d
Daniel Heater <daniel@heatertech.com>
2021-04-18 08:35:06 -0500

    Remove Meteor info

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Info } from './Info.jsx';
 
 export const App = () => (
   <div>
     <h1>Welcome to AL Family of Trees: Champion's Edition!</h1>
-    <Info/>
   </div>
 );
