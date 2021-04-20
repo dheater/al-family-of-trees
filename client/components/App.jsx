cef3679bc799361d5f20d98f324cd3ce167c356f
Daniel Heater <daniel@heatertech.com>
2021-04-19 19:12:44 -0500

    Make App a class component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ImageList from "./ImageList";
 
-export const App = () => (
-    <div>
-        <h1>Welcome to AL Family of Trees: Champion's Edition!</h1>
-        <p>Alabama Family of Trees: Champion's Edition is an app to help you learn about the trees you find in the state of Alabama.
-            It will even tell you about the <a href="https://forestry.alabama.gov/Pages/Management/Champion_Tree.aspx">biggest and best trees in the state including several national champions!</a>
-        </p>
-        <ImageList/>
-    </div>
-);
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to AL Family of Trees: Champion's Edition!</h1>
+                <p>Alabama Family of Trees: Champion's Edition is an app to help you learn about the trees you find in
+                    the
+                    state of Alabama.
+                    It will even tell you about the <a
+                        href="https://forestry.alabama.gov/Pages/Management/Champion_Tree.aspx">biggest and best trees
+                        in
+                        the state including several national champions!</a>
+                </p>
+                <ImageList/>
+            </div>
+        );
+    }
+}
